142486ebdbbff6bf1e41686a84aed4f34acb009d
RWTHsniper <goldenham89@gmail.com>
2020-10-07 01:10:58 +0200

    deep-q with cuda

--- practices/reinforcement learning/stock_env/env.py
+++ practices/reinforcement learning/stock_env/env.py
@@ -18,36 +18,50 @@ class TradingSPYEnv(gym.Env):
       - when selling, sell all the shares
       - when buying, buy as many as cash in hand allows
     """
-    def __init__(self, train_data_path='historySPY.csv', sma_len=[5], init_invest=10000):
+    def __init__(self, train_data_path='historySPY.csv', sma_len=[5], init_invest=10000, learning_rate=0.0002, gamma=0.98,
+                normalize_price = True, mode = 'train', train_test_split = 0.9):
         train_data = pd.read_csv(train_data_path, index_col = False, parse_dates= ['Date'])
         self.stock_price_history = train_data 
         self.current_step = 0 # The step in the data
         self.iteration = 0 # the iteration step in an episode
         self.init_invest = init_invest
         self.accumulated_profit = 0.0
+        self.normalize_price = normalize_price
 
         feature_dict = {'Date': self.stock_price_history['Date'],
                     'State': np.zeros(self.stock_price_history.shape[0], dtype=int),
-                    'accumulated_profit': np.zeros(self.stock_price_history.shape[0], dtype=float) ,
+                    'accumulated_profit': np.zeros(self.stock_price_history.shape[0], dtype=float), 
                     'portfolio_value': np.zeros(self.stock_price_history.shape[0], dtype=float),
                     'Close': self.stock_price_history['Close']
                     }
     
         # feature engineering. Put values like sma
-        feature = 'Close'
-        if isinstance([],list):
-            for sma in sma_len:
-                feature_dict[feature+'_'+str(sma)] = self.stock_price_history[feature].rolling(sma).mean()
+#        if sma_len([],list):
+            
+#            for sma in sma_len:
+#                feature_dict[feature+'_'+str(sma)] = self.stock_price_history[feature].rolling(sma).mean()
 #            self.stock_price_history[feature+'_'+str(sma)] = self.stock_price_history[feature].rolling(sma).mean()
                     
         self.stock_price_history.dropna(axis=0,inplace=True)
         self.stock_price_history.reset_index(drop=True,inplace=True)
-        self.features = pd.DataFrame(feature_dict).dropna(axis=0)
+
+        self.features = pd.DataFrame(feature_dict)
+        if isinstance(sma_len,list):
+            self._set_sma(sma_len)
+        self.features = self.features.dropna(axis=0)
         self.features.reset_index(drop=True,inplace=True)
-        self.features.portfolio_value.loc[self.current_step] = self.init_invest
     
-        self.end_step = self.features.shape[0]
-    
+        train_test_split_index = int(self.features.shape[0] * train_test_split)
+        if mode == 'train':
+            self.end_step = train_test_split_index
+        elif mode == 'test':
+            self.features.shape[0]
+            self.current_step = train_test_split_index
+            self.end_step = self.features.shape[0]
+
+        # Set up data and features
+        self.reset(current_step = self.current_step)
+            
         # action space
         # 0: short, 1: neutral, 2: long
         self.action_space = spaces.Discrete(3)
@@ -56,23 +70,38 @@ class TradingSPYEnv(gym.Env):
         # This contains features to make decisions
         self.observation_space = spaces.Box(low=0, high=np.inf, shape=(self.features.columns.shape[0] -1,), dtype=np.float16)
     
-    
+    def _set_sma(self, sma_len):
+        feature = 'Close'
+        for sma in sma_len:
+            col_name = feature+'_'+str(sma)
+            self.stock_price_history[col_name] = self.stock_price_history[feature].rolling(sma).mean()
+            self.features[col_name] = self.stock_price_history[feature].rolling(sma).mean()
+        
     def _get_observation(self):
         # return features at current step
         tmp = self.features.drop(columns=['Date']).loc[self.current_step].to_numpy()
         # state, portfolio_value, Close, smas
         return tmp
 
-    def reset(self):
+    def reset(self, current_step = None):
         self.iteration = 0 
         self.features['State'] = 1 # State:1 means market neutral
         self.features['portfolio_value'] = 0.0       
         self.features['accumulated_profit'] = 0.0
         
         # Set the current step to a random point within the data frame
-        self.current_step = random.randint(
-            0, int(self.features.shape[0] * 0.9))
+        if current_step is not None:
+            self.current_step = current_step
+        else:
+            self.current_step = random.randint(0, int(self.features.shape[0] * 0.9))
+            
         self.features['portfolio_value'].loc[self.current_step] = self.init_invest
+        
+        if self.normalize_price:
+            price = self.stock_price_history['Close'].loc[self.current_step]
+            for col in self.features.columns:
+                if 'Close' in col:
+                    self.features[col] = self.stock_price_history[col] / price
 
         return self._get_observation()
 
@@ -85,7 +114,8 @@ class TradingSPYEnv(gym.Env):
         if next_step == self.end_step: 
             # At the end, we have nothing to do
             done = True
-            return None, None, done, None
+            return None, None, done, {'accumulated_profit': self.accumulated_profit,
+                                       'iterations': self.iteration}
         
         col_name = 'Close'
         features = self.features
