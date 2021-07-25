63f9731879189c885e44e3edf87a9b4bbba95d65
RWTHsniper <goldenham89@gmail.com>
2021-07-26 00:15:03 +0200

    MC works fist time

--- practices/quant/mbs/mc.jl
+++ practices/quant/mbs/mc.jl
@@ -41,7 +41,7 @@ function get_h_t(a,b,gamma,k,r,t,T_asterisk)
     return res
 end
 
-function get_h_t(a,b,gamma,k,r::Vector,t,T_asterisk)
+function get_h_t(a,b,gamma,k,r::Union{Vector,SubArray},t,T_asterisk)
     ho_t = get_ho_t(a,b,t,T_asterisk)
     res = ho_t .+ gamma*max.(k.-r,0.0) # max(k-r,0)
     return res
@@ -73,7 +73,7 @@ e_dist = dist.Exponential(1) # exponential distribution about prepayment. Integr
 # Let's write a simulation code
 num_paths= 5
 annual_steps = 12 # steps per year
-num_paths= 4000
+num_paths= 1000
 annual_steps = 300 # steps per year
 dt = 1/annual_steps
 T = 10 # year
@@ -144,7 +144,7 @@ for current in 1:(num_steps-1)
     
     # compute harzard rate
     global h_t
-    h[:,next] = get_h_t(a,b,gamma,k,r[:,next],t,T_asterisk)
+    h[:,next] = get_h_t(a,b,gamma,k,view(r,:,next),t,T_asterisk)
 end
 
 println("Check whether CIR process simulation is reliable")
@@ -183,7 +183,6 @@ println("minimum prepayment step ", minimum(prepay_step), " total steps ", num_s
 # First, we need to compute m(0,T)
 P0 = 1.0 # presume initial mortgage price
 # m = 0.03 # mortgage rate
-c = 0.05 # coupon
 
 
 # function get_int_t_u_r(t_sim,r,t_ind,u_ind) 
@@ -192,13 +191,12 @@ c = 0.05 # coupon
 #     res = ni.integrate(t_sim[t_ind:u_ind], r[t_ind:u_ind])
 # end
 
-
 int_0_u_r = zeros(num_paths,num_steps) # int_0^(u) rs ds at each step
 int_0_u_h = zeros(num_paths,num_steps) # int_0^(u) hs ds at each step
 for i in 1:num_paths
     for j in 2:num_steps
-        int_0_u_r[i,j] = ni.integrate(t_sim[1:j], r[i,1:j])
-        int_0_u_h[i,j] = ni.integrate(t_sim[1:j], h[i,1:j])
+        int_0_u_r[i,j] = ni.integrate(view(t_sim,1:j), view(r,i,1:j))
+        int_0_u_h[i,j] = ni.integrate(view(t_sim,1:j), view(h,i,1:j))
     end
 end
 # for example, if you want to compute int_u1_u2_r, then compute int_0_u2_r - int_0_u1_r
@@ -226,7 +224,7 @@ function m_func(m0,t_sim,T,int_0_u_r_h,r) # 3.9
     d_num = zeros(size(t_sim))
     d_den = zeros(size(t_sim))
     for (j,u) in enumerate(t_sim)
-        r_u = r[:,j]
+        r_u = view(r,:,j)
         factor = (1-exp(-m0*(T-u)))
         d_num[j] = factor*get_R(int_0_u_r_h[:,j], r_u)
         d_den[j] = factor*get_Q(int_0_u_r_h[:,j])
@@ -240,23 +238,47 @@ end
 obj_fun = x -> (x - m_func(x,t_sim,T,int_0_u_r_h,r))^2
 res = Optim.optimize(obj_fun, 0.001,0.04)
 m = res.minimizer
+c = get_c(m,P0,T) # coupon (2.1)
 
 # Computation of M0 (3.1)
 M0 = zeros(num_paths)
 buffer = zeros(num_steps) # buffer to save intermediate result
 for i in 1:num_paths # looping over each path
     p_step = prepay_step[i] # prepayment step for i-th path
-    int_0_u_r_u = int_0_u_r[i,p_step]
+    t_view = view(t_sim, 1:p_step) # use view to avoid deepcopy
+    int_r = int_0_u_r[i,p_step]
     if p_step < num_steps # prepayment occured
-        discount = exp(-int_0_u_r_u)
+        discount = exp(-int_r)
         M0[i] = discount*get_P_no_prepay(P0,m,t_sim[p_step],T)
     end
     # compute cashflow from coupon
     # compute discounted c
-    for (j, t) in enumerate(t_sim[1:p_step]) # j-time step
-        discount = exp(-int_0_u_r_u)
+    for (j, t) in enumerate(t_view) # j-time step
+        discount = exp(-int_r)
         buffer[j] = discount
     end
-    M0[i] += c * ni.integrate(t_sim[1:p_step], buffer[1:p_step])
+    M0[i] += c * ni.integrate(t_view, view(buffer,1:p_step)) # mind deep copy
 end
 
+println("M0 at t=0 ",stats.mean(M0))
+
+# Another formulation for M
+
+
+
+
+# Test for M0
+# The following expression should be zero 3.6
+t = 0.0
+t_ind = 1
+# buffer = zeros(length(t_sim)-t_ind+1)
+buffer = zeros(length(t_sim))
+for (u_ind,u) in enumerate(t_sim)
+    factor = (1.0-exp(-m*(T-u)))/(1.0-exp(-m*(T-t)))
+    int_view = view(int_0_u_r_h,:,t_ind:u_ind)
+    r_view = view(r,:,u_ind)
+    factor2 = m * get_Q(int_view) - get_R(int_view, r_view)
+    buffer[u_ind] = factor*factor2
+end
+int_test = ni.integrate(t_sim, buffer)
+println("int_test is supposed to be 0 at t=0", int_test)
